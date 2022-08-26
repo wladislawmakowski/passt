ebf8e8a3b7f1883efb4908e4bbe869c65c4c2f31
Wladislaw Makowski <42301441+wladislawmakowski@users.noreply.github.com>
2022-08-26 21:38:18 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
-mkdir /home/$USER/Security && cd /home/$USER/Security
+mkdir ~/Security && cd ~/Security
 
 git clone https://github.com/wladislawmakowski/classical-cryptography.git
 git clone https://github.com/wladislawmakowski/passt.git
+cd passt
+
+touch key.passt password.passt
+chown root ~/Security
+
+gcc main.c ../classical-cryptography/source/implementations/otp.c -o main
 
 apt install xclip keepassxc
 cd passt && chmod +x main passt
-"export PATH="/home/$USER/Security/passt:$PATH"" >> ~/.bashrc
+"export PATH="~/Security/passt:$PATH"" >> ~/.bashrc
 "alias sudo='sudo -E env "PATH=$PATH"' << ~/.bashrc
 
 source ~/.bashrc
