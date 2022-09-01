ead467c355724500c1dc418d5f55e8e47eccd72b
Wladislaw Makowski <42301441+wladislawmakowski@users.noreply.github.com>
2022-09-01 09:54:18 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ gcc main.c ../classical-cryptography/source/implementations/otp.c -o main
 apt install xclip keepassxc
 cd passt && chmod +x main passt
 "export PATH="~/Security/passt:$PATH"" >> ~/.bashrc
-"alias sudo='sudo -E env "PATH=$PATH"' << ~/.bashrc
+"alias sudo='sudo -E env "PATH=$PATH"' >> ~/.bashrc
 
 source ~/.bashrc
 echo -e "\n[ PASST INSTALLED ]\n"
